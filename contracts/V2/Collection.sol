e215860644130bf32bfc645d2fe9acfa0b80dc58
Can ARIKAN <can.arikan@valensas.com>
2023-04-17 20:21:46 +0300

    Add Categories

--- contracts/V2/Collection.sol
+++ contracts/V2/Collection.sol
@@ -10,14 +10,24 @@ contract Collection is ERC721URIStorage {
     Counters.Counter private _tokenIds;
 
     address _owner;
+    string[] categories;
 
     modifier onlyOwner() {
         require(msg.sender == _owner);
         _;
     }
 
-    constructor(string memory collectionName, string memory collectionSymbol) ERC721(collectionName, collectionSymbol) {
+    constructor(string memory collectionName, string memory collectionSymbol, string[] memory category) ERC721(collectionName, collectionSymbol) {
+        categories = category;
         _owner = msg.sender;
+    }
+
+    function getCategory() onlyOwner public view returns (string[] memory) {
+        return categories;
+    }
+
+    function setCategory(string[] memory cat) onlyOwner public {
+        categories = cat;
     }
     
     function createToken(string memory tokenURI) onlyOwner public payable returns (uint256) {
@@ -31,6 +41,8 @@ contract Collection is ERC721URIStorage {
     function owner() public view returns(address) {
         return _owner;
     }
+
+
 }
 
 abstract contract CollectionHolder {
@@ -38,13 +50,15 @@ abstract contract CollectionHolder {
     Counters.Counter private _collectionIds;
 
     uint256 listingPrice;
-
+    string[] availableCategories;
+    
     Collection[] collectionsArray;
 
-    function createCollection(string memory collectionName, string memory collectionSymbol) public payable {
+    function createCollection(string memory collectionName, string memory collectionSymbol, string[] memory categories) public payable {
         require(_checkBalance(msg.value));
         require(findCollection(msg.sender, collectionName) == (_collectionIds.current() + 1));
-        Collection newCollection = new Collection(collectionName, collectionSymbol);
+        require(checkCategories(categories));
+        Collection newCollection = new Collection(collectionName, collectionSymbol, categories);
         collectionsArray.push(newCollection);
         _collectionIds.increment();
     }
@@ -72,5 +86,19 @@ abstract contract CollectionHolder {
 
     function compare(string memory str1, string memory str2) private pure returns (bool) {
         return keccak256(abi.encodePacked(str1)) == keccak256(abi.encodePacked(str2));
+    }
+
+    function checkCategories(string[] memory categories) private view returns (bool) {
+        for (uint256 i = 0; i < categories.length; i++) {
+            if (!isInListStrStr(categories[i], availableCategories)) return false;
+        }
+        return true;
+    }
+
+    function isInListStrStr(string memory str, string[] memory lst) private pure returns (bool) {
+        for (uint256 i = 0; i < lst.length; i++) {
+            if (compare(lst[i], str)) return true;
+        }
+        return false;
     }
 }
